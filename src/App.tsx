664474bd235b7cbe5f85b6f0c4e8e49d7c80e0b6
Nicolas Bazille <oltodo@msn.com>
2020-11-20 16:57:36 +0100

    Basically display numbers randomly with speech

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,89 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import shuffle from "lodash/shuffle";
+import range from "lodash/range";
+import writtenNumber from "written-number";
+import classes from "./App.module.css";
+
+const max = 500;
+
+function getRandNumber() {
+  return shuffle(range(0, max));
+}
+
+let voices: SpeechSynthesisVoice[] | null = null;
+
+window.speechSynthesis.onvoiceschanged = () => {
+  voices = window.speechSynthesis
+    .getVoices()
+    .filter((voice) => voice.lang === "fr-FR");
+};
+
+function say(text: string) {
+  return new Promise((resolve) => {
+    var msg = new SpeechSynthesisUtterance();
+    msg.text = text;
+    msg.onend = resolve;
+
+    if (voices) {
+      msg.voice = voices[voices.length - 1];
+    }
+
+    window.speechSynthesis.speak(msg);
+  });
+}
 
 function App() {
+  const [mode, setMode] = useState(0);
+  const [currentIndex, setCurrentIndex] = useState(-1);
+  const [randNumbers, setRandNumbers] = useState(getRandNumber());
+
+  useEffect(() => {
+    const increment = async (event: KeyboardEvent) => {
+      if (event.code !== "Space") {
+        return;
+      }
+
+      if (currentIndex >= 0) {
+        await say(writtenNumber(randNumbers[currentIndex], { lang: "fr" }));
+      }
+
+      if (currentIndex + 1 === randNumbers.length) {
+        setRandNumbers(getRandNumber());
+        setMode(mode === 1 ? 0 : 1);
+        setCurrentIndex(0);
+      } else {
+        setCurrentIndex(currentIndex + 1);
+      }
+    };
+
+    window.addEventListener("keydown", increment);
+
+    return () => {
+      window.removeEventListener("keydown", increment);
+    };
+  }, [currentIndex, mode, randNumbers]);
+
+  if (currentIndex === -1) {
+    return null;
+  }
+
+  const text =
+    mode === 1
+      ? randNumbers[currentIndex]
+      : writtenNumber(randNumbers[currentIndex], { lang: "fr" });
+
+  const fontSize = Math.min(8, 120 / text.length);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      <span
+        className={classes.number}
+        style={{
+          fontSize: fontSize + "vw",
+        }}
+      >
+        {text}
+      </span>
     </div>
   );
 }
